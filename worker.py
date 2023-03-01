31f3b101273d62583dca7ee3f645d3c10a01f569
Abdulsamet ŞAHİN <abdulsametsah@gmail.com>
2023-03-01 08:38:07 +0300

    remove prints

--- worker.py
+++ worker.py
@@ -11,14 +11,16 @@ import pika
 def worker():
     while True:
         try:
-            credentials = pika.PlainCredentials(rabbitmq_config['user'], rabbitmq_config['password'])
+            credentials = pika.PlainCredentials(
+                rabbitmq_config['user'], rabbitmq_config['password'])
             parameters = pika.ConnectionParameters(
                 host=rabbitmq_config['host'], port=rabbitmq_config['port'], credentials=credentials, heartbeat=120)
             connection = pika.BlockingConnection(parameters)
             consumer_channel = connection.channel()
             publisher_channel = connection.channel()
 
-            queue_arguments = {'x-queue-mode': 'lazy', 'x-max-length': 100000, 'x-overflow': 'reject-publish'}
+            queue_arguments = {
+                'x-queue-mode': 'lazy', 'x-max-length': 100000, 'x-overflow': 'reject-publish'}
             consume_queue = consumer_channel.queue_declare(queue='company_link', durable=True,
                                                            arguments=queue_arguments)
 
@@ -28,25 +30,21 @@ def worker():
 
             consumer_channel.basic_qos(prefetch_count=1)
 
-            print(f"[x] [ScrapeCompany] Waiting for messages in {consume_queue.method.queue}. To exit press CTRL+C")
+            print(
+                f"[x] [ScrapeCompany] Waiting for messages in {consume_queue.method.queue}. To exit press CTRL+C")
             for method_frame, properties, body in consumer_channel.consume(consume_queue.method.queue):
-                print(f"[x] [ScrapeCompany] Received {body}")
                 url = f"https://www.gongsi.com.cn/detail/{body.decode('utf-8')}"
-                print(f"[x] [ScrapeCompany] Scraping {url}")
                 scraper = ScrapeCompany(url)
                 company_data = scraper.scrape()
                 if company_data == 500:
-                    print(f"[x] [ScrapeCompany] 500 error")
                     company_data = None
 
                 if company_data is not None:
                     publisher_channel.basic_publish(exchange='', routing_key=publish_queue.method.queue,
                                                     body=json.dumps(company_data))
-                    print("[x] [ScrapeCompany] Done")
-                else:
-                    print(f"[x] [ScrapeCompany] No data")
 
-                consumer_channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+                consumer_channel.basic_ack(
+                    delivery_tag=method_frame.delivery_tag)
 
         except KeyboardInterrupt:
             print("[x] [ScrapeCompany] Closing connection")
